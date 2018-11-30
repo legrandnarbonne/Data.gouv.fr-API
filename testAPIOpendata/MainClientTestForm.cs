ecce5de1cc589cb37d9d06ee3375caa4b84eab0d
JD <j.dapot@legrandnarbonne.com>
2018-11-30 17:19:32 +0100

    .

--- testAPIOpendata/MainClientTestForm.cs
+++ testAPIOpendata/MainClientTestForm.cs
@@ -28,7 +28,7 @@ namespace testAPIOpendata
             comboBox1.Items.Add("DataSet");
 
             comboBox1.SelectedIndex = 0;
-            txtKey.Text = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiNTliNjdkNzA4OGVlMzgwODEyY2RlMGZlIiwidGltZSI6MTUzOTIzODE5NC4yMzMzNDZ9.Tqzd7WocTdXtCCEA9P3AlkCX-Fdbrr3vXZ7QVm2GOEQ";
+            txtKey.Text = "";
 
         }
 
